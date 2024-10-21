cd54d59996fc05cca9d2662ec6aea59e26696915
wael-benslimen <waelbenslimen1111@gmail.com>
2024-10-21 17:25:02 +0100

    Create forloopbasic.py

---
+++ fundamentals/fundamentals/testing python/forloopbasic.py
@@ -0,0 +1,27 @@
+for i in range(151):
+    print(i)
+for i in range(5,1000):
+    if i%5 == 0 :
+        print(i)
+for i in range(1,101):
+    if i%10 == 0:
+      print("coding dojo")
+    elif(i%5 == 0):
+        print("coding")
+x = 0
+for i in range(500000):
+    if i%2 != 0:
+        x = x + i
+print(x)
+for i in range(2018,-1,-4):
+    print(i)
+lownum = int(input("iner low number"))
+hightnum = int(input("enter the high number"))
+mult = int(input("enter mult"))
+for i in range(lownum,hightnum):
+    if i%mult == 0:
+        print(i)
+      
+ 
+ 
+    
