52da2e0bddd47b2f78d3f01ecf7f18b40e498b69
wael-benslimen <waelbenslimen1111@gmail.com>
2024-10-21 17:54:55 +0100

    chage

--- fundamentals/fundamentals/functions_basic_i/functions_basic_i.py
+++ fundamentals/fundamentals/functions_basic_i/functions_basic_i.py
@@ -1,61 +1,66 @@
-#1
-def number_of_food_groups():
-    return 5
-print(number_of_food_groups())
+
+  1
+  def number_of_food_groups():
+      return 5
+  print(number_of_food_groups())
+   #it will log 5
 
 
-#2
-def number_of_military_branches():
-    return 5
-print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+  2
+  def number_of_mil#itary_branches():
+      return 5
+  print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_mil#itary_branches())
+  'number_of_days_in_a_week_silicon_or_triangle_sides' is not defined
+
+ 3
+ def number_of_books_on_hold():
+     return 5
+     return 10
+ print(number_of_books_on_hold())
+  #it ll log 5
 
 
-#3
-def number_of_books_on_hold():
-    return 5
-    return 10
-print(number_of_books_on_hold())
+ 4
+ def number_of_fingers():
+     return 5
+     print(10)
+ print(number_of_fingers())
+  #it ll log 5
+
+ 5
+ def number_of_great_lakes():
+     print(5)
+ x = number_of_great_lakes()
+ print(x)
+  #it ll log 5
+
+ 6
+ def add(b,c):
+     print(b+c)
+ print(add(1,2) + add(2,3))
+ #it ll log none
+
+ 7
+ def concatenate(b,c):
+     return str(b)+str(c)
+ print(concatenate(2,5))
+ #it will log 25
 
 
-#4
-def number_of_fingers():
-    return 5
-    print(10)
-print(number_of_fingers())
+ 8
+ def number_of_oceans_or_fingers_or_continents():
+     b = 100
+     print(b)
+     if b < 10:
+         return 5
+     else:
+         return 10
+     return 7
+ print(number_of_oceans_or_fingers_or_continents())
+ #it will return 10
 
 
-#5
-def number_of_great_lakes():
-    print(5)
-x = number_of_great_lakes()
-print(x)
-
-
-#6
-def add(b,c):
-    print(b+c)
-print(add(1,2) + add(2,3))
-
-
-#7
-def concatenate(b,c):
-    return str(b)+str(c)
-print(concatenate(2,5))
-
-
-#8
-def number_of_oceans_or_fingers_or_continents():
-    b = 100
-    print(b)
-    if b < 10:
-        return 5
-    else:
-        return 10
-    return 7
-print(number_of_oceans_or_fingers_or_continents())
-
-
-#9
+9
 def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
     if b<c:
         return 7
@@ -65,16 +70,16 @@ def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
 print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
 print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
 print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+#it will log 7 , 14,21
 
-
-#10
+10
 def addition(b,c):
     return b+c
     return 10
 print(addition(3,5))
+#it will log 8
 
-
-#11
+11
 b = 500
 print(b)
 def foobar():
@@ -83,9 +88,10 @@ def foobar():
 print(b)
 foobar()
 print(b)
-
-
-#12
+#it will log 500
+#it will log 500
+#it will log 300
+12
 b = 500
 print(b)
 def foobar():
@@ -95,9 +101,11 @@ def foobar():
 print(b)
 foobar()
 print(b)
+#it will log 500
+#it will log 500
+#it will log 300
 
-
-#13
+13
 b = 500
 print(b)
 def foobar():
@@ -107,17 +115,9 @@ def foobar():
 print(b)
 b=foobar()
 print(b)
-
-
-#14
-def foo():
-    print(1)
-    bar()
-    print(2)
-def bar():
-    print(3)
-foo()
-
+#it will log 500
+#it will log 500 
+#it will log 300
 
 #15
 def foo():
@@ -130,3 +130,4 @@ def bar():
     return 5
 y = foo()
 print(y)
+#it will log 1 , 3, 5,10
