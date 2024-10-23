39f9836965e5424a08c89bd3671b22f8463d3f00
wael-benslimen <waelbenslimen1111@gmail.com>
2024-10-23 13:27:29 +0100

    Create chaining_methods.py

---
+++ fundamentals/fundamentals/chaining methods/chaining_methods.py
@@ -0,0 +1,31 @@
+class user:
+    def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+        
+    def display_info(self):
+        print(f"first name is{self.first_name}")
+        print(f"last name is {self.last_name}")
+        print(f"emai : {self.email}")
+        print(f"age : {self.age}")
+        print(f"reward mumber status : {self.is_rewards_member}")
+        print(f"gold cad points = {self.gold_card_points}")
+        
+    def enroll(self):
+        self.is_rewards_member = True
+        self.gold_card_points = 200
+        
+    def spend_points(self, amount):
+        self.gold_card_points = self.gold_card_points - amount
+
+x = user("wael", "benslimen", "waelbeslimen111@gmail.com", "21")
+y = user("jane", "doe", "janedoe@gmail.com", "21")
+z = user("eren", "yeagar", "eren111@gmail.com", "25")
+
+x.enroll().spend_points(50).display_info()
+y.enroll().spend_points(80).display_info()
+z.display_info()
